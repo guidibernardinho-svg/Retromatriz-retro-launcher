56b686974ffe7aed13dfde0157cfec3a6c5bd724
guidibernardinho-svg <guidibernardinho@gmail.com>
2026-04-18 18:51:02 -0300

    Create ViewController.mm

---
+++ ios/ViewController.mm
@@ -0,0 +1,14 @@
+// ios/ViewController.mm
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Emulador iniciado!");
+}
+
+@end
